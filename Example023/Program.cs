338c41c16c17cf488b24e3ab911295e267ef07e4
Kiesaro <freestylerspb@gmail.com>
2022-12-26 04:14:56 +0300

    Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1 ; i < number ; i++)
+{
+    double cube = Math.Pow(i,3);
+    Console.Write(cube + " , " );
+}
+ Console.Write (Math.Pow(number,3));
